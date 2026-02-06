9c981e72c1a968ec175ee5adb9158ec3b4ec77e1
AnshKumar <tech.anshkumar.28@gmail.com>
2026-02-06 21:26:08 +0530

    DAY11

--- DAY 11/script.js
+++ DAY 11/script.js
@@ -1,16 +1,26 @@
-// function handelClick(){
+// function handleClick(){
 //     document.querySelector("#head").style.color="red";
 
-//     document.getElementById("btn").textContent="color changed";
-
+//     document.getElementById("btn").textContent="color changed"
 // }
 
-const handelClick = () => {
+// Q2.
+// const handleClick = () => {
+//     const image = document.createElement("img");
+//     image.setAttribute("src", "https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
+
+//     image.setAttribute("alt", "kr mangalam");
+
+//     document.getElementById("content").append(image);
+// }
+
+
+
+ document.getElementById("btn").addEventListener("click",()=>{
     const image = document.createElement("img");
     image.setAttribute("src", "https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
 
-    image.setAttribute("alt", "KR Mangalam");
+    image.setAttribute("alt", "kr mangalam");
 
     document.getElementById("content").append(image);
-
-}
+})
